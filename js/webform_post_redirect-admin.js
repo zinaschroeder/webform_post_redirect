19df971a6c768e62f877bc2027bb18acebbf8f39
Zina Schroeder <zina.schroeder@gmail.com>
2017-03-27 01:34:55 -0500

    Adding admin settings fields to modify redirect page and POST output. (no implementation logic yet)

--- js/webform_post_redirect-admin.js
+++ js/webform_post_redirect-admin.js
@@ -8,6 +8,7 @@ jQuery(document).ready(function($){
   // Start in correct toggle display
   toggle_post_redirect_element($('#edit-redirect input:checked').val(), 'url', '.form-item-redirect-post');
   toggle_post_redirect_element($('#edit-redirect-post').is(':checked'), true, '#edit-components.webform-component-select-table');
+  toggle_post_redirect_element($('#edit-redirect-post').is(':checked'), true, '#edit-page-settings');
 
   // When the radio changes, check if the checkbox and components should be hidden
   $('#edit-redirect input[type=radio]').on('change', function() {
@@ -17,18 +18,22 @@ jQuery(document).ready(function($){
 
       if ($('#edit-redirect-post').is(':checked')) {
         $('#edit-components.webform-component-select-table').show();
+        $('#edit-page-settings').show();
       } else {
         $('#edit-components.webform-component-select-table').hide();
+        $('#edit-page-settings').hide();
       }
     } else {
       $('.form-item-redirect-post').hide();
       $('#edit-components.webform-component-select-table').hide();
+      $('#edit-page-settings').hide();
     }
   });
 
   // When the checkbox changes, check if components should be hidden
   $('#edit-redirect-post').on('change', function() {
     toggle_post_redirect_element($(this).is(':checked'), true, '#edit-components.webform-component-select-table');
+    toggle_post_redirect_element($('#edit-redirect-post').is(':checked'), true, '#edit-page-settings');
   });
 
   // Trigger display of element based on trigger and truth case
