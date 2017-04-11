e7aa689f94cf6c278fcac86cbac59d8a5fc26510
Zina Schroeder <zina.schroeder@gmail.com>
2017-04-10 19:12:10 -0500

    Updating template to rename header as page_title and putting page text message in div tag.

--- templates/webform-post-redirect-page.tpl.php
+++ templates/webform-post-redirect-page.tpl.php
@@ -5,17 +5,19 @@
    *
    * Available variables:
    * $form: The hidden form that sends webform data
-   * $header: The page header
+   * $page_title: The page title
    * $message: The page text
    */
 ?>
 <div id='webform_post_redirect_page'>
-  <h1><?php print render($header); ?></h1>
+  <h1><?php print render($page_title); ?></h1>
 
   <img src="<?php echo base_path() . drupal_get_path('module', 'webform_post_redirect'); ?>/images/loading.gif" class="centered" />
   <br/>
 
-  <p><?php print render($message); ?></p>
+  <div id='webform_post_redirect_message'>
+    <?php print render($message); ?>
+  </div>
 
   <?php print render($form); ?>
 </div>
